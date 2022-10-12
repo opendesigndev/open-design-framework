ef7389e2773ff9ca9dd3a18b71c4010894f437dd
Isabella Skořepová <isabella@skorepova.info>
2022-10-12 10:22:44 +0200

    eslint + publish config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,18 @@
 module.exports = {
   extends: ["react-app", "prettier"],
+  plugins: ["simple-import-sort"],
+  parserOptions: {
+    project: ["./tsconfig.json", "packages/*/tsconfig.json"],
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { prefer: "type-imports" },
+    ],
+    "@typescript-eslint/consistent-type-exports": "warn",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+  ignorePatterns: ["packages/*/dist/**/*"],
+  root: true,
 };
