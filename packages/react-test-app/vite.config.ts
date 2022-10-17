16be664dfd793635eef1ad4a9c300a505f9277a6
Isabella Skořepová <isabella@skorepova.info>
2022-10-17 18:17:12 +0200

    fix build

--- packages/react-test-app/vite.config.ts
+++ packages/react-test-app/vite.config.ts
@@ -1,28 +1,39 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import packageJson from "./package.json";
 // @ts-expect-error
 import fs from "node:fs";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+import packageJson from "./package.json";
 
 // TODO: remove once usage starts giving errors (= vite fixes its typedefs)
 function hackDefault<T>(v: { default: T }): T {
   return v as any;
 }
 
+const localDeps = Object.entries(packageJson.dependencies)
+  .filter(([k, v]) => k.startsWith("@avocode/") && v.startsWith("workspace:"))
+  .map(([k]) => k);
+
 // Resolve all local packages directly so that development does not require
 // running typescript watcher.
 const alias = Object.fromEntries(
-  Object.entries(packageJson.dependencies)
-    .filter(([k, v]) => k.startsWith("@avocode/") && v.startsWith("workspace:"))
-    .map(([k, v]) => {
-      const nox = new URL(`../${k.slice(9)}/index.ts`, import.meta.url)
-        .pathname;
-      return [k, fs.existsSync(nox) ? nox : nox + "x"];
-    })
+  localDeps.map((k) => {
+    const nox = new URL(`../${k.slice(9)}/index.ts`, import.meta.url).pathname;
+    return [k, fs.existsSync(nox) ? nox : nox + "x"];
+  })
 );
 
 export default defineConfig({
-  resolve: { alias },
+  optimizeDeps: { exclude: localDeps },
+  resolve: {
+    alias: {
+      ...alias,
+      // bypass package.json['import'] for @opendesign/universal too
+      "#env": new URL("../opendesign-universal/src/env-dom.ts", import.meta.url)
+        .pathname,
+    },
+  },
   server: {
     fs: { allow: ["../.."] },
   },
