cd69a476f340ffeccd73eb4df79f0d566d2c5292
Isabella Skořepová <isabella@skorepova.info>
2023-04-26 14:49:31 +0200

    enable embed-fonts

--- packages/opendesign/cli/cli.ts
+++ packages/opendesign/cli/cli.ts
@@ -10,13 +10,13 @@ const commands: {
 } = {
   open: () => import("./open.js"),
   convert: () => import("./convert.js"),
+  "embed-fonts": () => import("./embed-fonts.js"),
 };
 
 if (process.env.NODE_ENV !== "production") {
   // Work in progress commands which are not enabled yet in build published to npm
   Object.assign(commands, {
     pack: () => import("./pack.js"),
-    "embed-fonts": () => import("./embed-fonts.js"),
   });
 }
 
